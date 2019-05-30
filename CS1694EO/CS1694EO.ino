a67015d40a88008a37b089504273395ec8ab13d6
radx64 <radekteam@gmail.com>
2019-05-30 19:47:16 +0200

    Adding some letters

--- CS1694EO/CS1694EO.ino
+++ CS1694EO/CS1694EO.ino
@@ -8,19 +8,24 @@
 CS1694EO driver(CLOCK_PIN, DATA_PIN, STB_PIN);
 DVD_4350_panel lcd_panel(driver);
 
-int counter = 0;
-char counter_str[10];
+const char TEXT_SIZE = 58;
+const char BUFFER_SIZE = 7;
+char text_marquee[TEXT_SIZE] = "LCD DEMO BY RADX64 abcdefghijklmnopqrstuvwxyz 0123456789";
+char text_buffer[7] = {};
+int position = 0;
 
 void setup() 
 {
   driver.init(CS1694EO::InitMode::M7_GRIDS_10_SEGS);
-  driver.set_brightness(CS1694EO::BrightnessLevel::BRIGHTNESS_LEVEL_3);
+  driver.set_brightness(CS1694EO::BrightnessLevel::BRIGHTNESS_LEVEL_7);
 }
   
 void loop() 
 {
-  itoa(counter, counter_str, 10);
-  write_text_on_display(counter_str, 7); //actualy write to local buffer and then (in a loop below) send it
+  memcpy(text_buffer, text_marquee + position, position+BUFFER_SIZE>=TEXT_SIZE-1?0:BUFFER_SIZE);
+  write_text_on_display(text_buffer, BUFFER_SIZE); //actualy write to local buffer and then (in a loop below) send it
   for(int i=0x00; i<CS1694EO::DISPLAY_RAM_SIZE ; ++i) driver.write_to_ram(i, data[i]);
-  counter+=1;
+  position+=1;
+  if (position >= TEXT_SIZE-1) position = 0;
+  delay(250);
 }
--- CS1694EO/DVD_4350_panel.hpp
+++ CS1694EO/DVD_4350_panel.hpp
@@ -42,6 +42,7 @@ const int spinner_icon_7 = 0x02;
  */
 
                       //a,b,c,d,e,f,g,
+const bool segs_space[7] = {0,0,0,0,0,0,0};
 const bool segs_0[7] = {1,1,1,1,1,1,0};
 const bool segs_1[7] = {0,1,1,0,0,0,0};
 const bool segs_2[7] = {1,1,0,1,1,0,1};
@@ -52,6 +53,32 @@ const bool segs_6[7] = {1,0,1,1,1,1,1};
 const bool segs_7[7] = {1,1,1,0,0,0,0};
 const bool segs_8[7] = {1,1,1,1,1,1,1};
 const bool segs_9[7] = {1,1,1,1,0,1,1};
+const bool segs_a[7] = {1,1,1,0,1,1,1};
+const bool segs_b[7] = {0,0,1,1,1,1,1};
+const bool segs_c[7] = {1,0,0,1,1,1,0};
+const bool segs_d[7] = {0,1,1,1,1,0,1};
+const bool segs_e[7] = {1,0,0,1,1,1,1};
+const bool segs_f[7] = {1,0,0,0,1,1,1};
+const bool segs_g[7] = {1,0,1,1,1,1,0};
+const bool segs_h[7] = {0,1,1,0,1,1,1};
+const bool segs_i[7] = {0,1,1,0,0,0,0};
+const bool segs_j[7] = {0,1,1,1,0,0,0};
+const bool segs_k[7] = {0,1,1,0,1,1,1};
+const bool segs_l[7] = {0,0,0,1,1,1,0};
+const bool segs_m[7] = {0,0,1,0,1,0,1};
+const bool segs_n[7] = {0,0,1,0,1,0,1};
+const bool segs_o[7] = {0,0,1,1,1,0,1};
+const bool segs_p[7] = {1,1,0,0,1,1,1};
+const bool segs_q[7] = {1,1,1,0,0,1,1};
+const bool segs_r[7] = {0,0,0,0,1,0,1};
+const bool segs_s[7] = {1,0,1,1,0,1,1};
+const bool segs_t[7] = {0,0,0,1,1,1,1};
+const bool segs_u[7] = {0,0,1,1,1,0,0};
+const bool segs_v[7] = {0,0,1,1,1,0,0};
+const bool segs_w[7] = {0,0,1,1,1,0,0};
+const bool segs_x[7] = {0,1,1,0,1,1,1};
+const bool segs_y[7] = {0,1,1,1,0,1,1};
+const bool segs_z[7] = {1,1,0,1,1,0,1};
 
 const int a_segment_address = 0x00;
 const int b_segment_address = 0x02;
@@ -60,6 +87,7 @@ const int d_segment_address = 0x06;
 const int e_segment_address = 0x08;
 const int f_segment_address = 0x0A;
 const int g_segment_address = 0x0C;
+
 const int digit_1 = 0x01;
 const int digit_2 = 0x02;
 const int digit_3 = 0x80; // yea third digit is way further then rest
@@ -107,8 +135,9 @@ void write_text_on_display(char* text, int len)
 {
   for(int index = 0; index < len; ++index)
   {
-    switch(text[index])
+    switch(tolower(text[index]))
     {
+      case ' ' : write_(segs_space, index); break;
       case '0' : write_(segs_0, index); break;
       case '1' : write_(segs_1, index); break;
       case '2' : write_(segs_2, index); break;
@@ -119,6 +148,32 @@ void write_text_on_display(char* text, int len)
       case '7' : write_(segs_7, index); break;
       case '8' : write_(segs_8, index); break;
       case '9' : write_(segs_9, index); break;
+      case 'a' : write_(segs_a, index); break;
+      case 'b' : write_(segs_b, index); break;
+      case 'c' : write_(segs_c, index); break;
+      case 'd' : write_(segs_d, index); break;
+      case 'e' : write_(segs_e, index); break;
+      case 'f' : write_(segs_f, index); break;
+      case 'g' : write_(segs_g, index); break;
+      case 'h' : write_(segs_h, index); break;
+      case 'i' : write_(segs_i, index); break;
+      case 'j' : write_(segs_j, index); break;
+      case 'k' : write_(segs_k, index); break;
+      case 'l' : write_(segs_l, index); break;
+      case 'm' : write_(segs_m, index); break;
+      case 'n' : write_(segs_n, index); break;
+      case 'o' : write_(segs_o, index); break;
+      case 'p' : write_(segs_p, index); break;
+      case 'q' : write_(segs_q, index); break;
+      case 'r' : write_(segs_r, index); break;
+      case 's' : write_(segs_s, index); break;
+      case 't' : write_(segs_t, index); break;
+      case 'u' : write_(segs_u, index); break;
+      case 'v' : write_(segs_v, index); break;
+      case 'w' : write_(segs_w, index); break;
+      case 'x' : write_(segs_x, index); break;
+      case 'y' : write_(segs_y, index); break;
+      case 'z' : write_(segs_z, index); break;
       default : continue;
     }
   }
